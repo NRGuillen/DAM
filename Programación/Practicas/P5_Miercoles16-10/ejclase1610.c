c34d0b2f64e16e8560d6c45cd7ff025ba8a3a90d
Guillen <ruben.guillen.dosa@gmail.com>
2024-10-16 11:13:16 +0200

    Update ejclase1610.c
    Si

--- Programación/Practicas/P5_Miercoles16-10/ejclase1610.c
+++ Programación/Practicas/P5_Miercoles16-10/ejclase1610.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>	
 #include <math.h>
 
+//declaro todas las funciones que luego llamaremos en el main para hacer los calculos
+
 int es_mayuscula(char letra);
 void calcular_factorial();
 int encontrar_maximo(int num1, int num2);
@@ -10,6 +12,8 @@ void intercambiar_numeros();
 double calcular_potencia(double base, int exponente);
 
 int main (){
+	
+	//declaro opciones que sera el numero que introduzca para seleccionar una opcion 1-7
 	
 	int opciones;
 
@@ -21,13 +25,19 @@ int main (){
 	printf("5.Intercambiar dos números.\n");
 	printf("6.Calcular la potencia de un número.\n");
 	printf("7.Salir.\n");
-	printf("Opción:\n");
-
+	printf("Opción: ");
+	
+	//se escanea el numero que hemos puesto
+	
 	scanf("%d", &opciones);
 	
+	//se selecciona un caso dependiendo del numero que seleccionemos
+
 	switch(opciones){
 		case 1:{ 
-		
+	
+		//se declara letra, para el scanf y se llama a la funcion es_mayuscula, que es la que realizara los calculos en una funcion aparte de int.
+
 		char letra;
 		
 		printf("Introduce un carácter: ");
@@ -37,6 +47,7 @@ int main (){
 		
 	break;
 	}	
+		//se declara numero para el scanf y se llama a la funcion calcular_factorial fuera del main
 
 		case 2:{
 		
@@ -50,10 +61,12 @@ int main (){
 	break;
         }
 	
+		//se declaran 2 variables num1 y numb2 para calcular con esos numeros en la funcion encontrar_maximo(num1,num2)
+
 		case 3:{
 
 		int num1, num2;
-		
+			
 		printf("Introduce el primer número: ");
 		scanf("%d", &num1);
 		printf("Introduce el segundo número: ");
@@ -66,6 +79,7 @@ int main (){
 	break;
         }
 
+		// se declaran 2 variables para cacular con esos numeros en convertir_celsius_a_fahrenheit
 		case 4:{
 
 		double celsius, fahrenheit;
@@ -79,6 +93,8 @@ int main (){
 	
 	break;
 	}
+		
+		//declaro 2 variables para calcular con esos numeros en intercambiar_numeros
 
 		case 5:{
 		
@@ -91,9 +107,11 @@ int main (){
 
 		intercambiar_numeros;
 
-		printf("Después de intercambiar, el primer número es el %.2f, el segundo número es el %.2f\n", num2, num1);
+		printf("Después de intercambiar, el primer número es el %.2f, el segundo número es el %.2f\n", num1, num2);
 	break;
        }
+
+		//declaro en double base, resultado (me lo pide el ejercicio) y el exponente para calcularlo en calcular_potencia(base,exponente)
 
 		case 6:{
 		
@@ -126,18 +144,21 @@ return 0;
 	}
 
 int es_mayuscula(char letra){
+		
+	//este codigo se leeria como si la letra esta entre la A mayuscula y la Z mayuscula la letra es mayuscula, y viceversa pero con minusculas y si no es una letra daria error
 
 	if(letra >='A' && letra <= 'Z'){
 	printf("El carácter %c es una mayúscula\n", letra);
 	return 1;
-	}else{
-	printf("El carácter %c no es una mayúscula\n", letra);
+	}else if(letra >='a' && letra <= '<'){
+	printf("El carácter %c es una minuscula\n", letra);
 	return 0;
+	}else{
+	printf("No has introducido una letra\n");
 	}
-}
-
+	}
 void calcular_factorial(){
-//no se como expresar la formula factorial para que calcule un numero entero, y lo que he buscado por internet son cosas que no hemos dado todavia
+//no se como expresar la formula factorial para que calcule un numero entero, y lo que he buscado por internet son cosas que no hemos dado todavia y no comprendo realmente como funcionan.
 printf("A espera de explicacion en clase\n");
 }
 
@@ -145,7 +166,7 @@ int encontrar_maximo(int num1, int num2){
 
 	if(num1 > num2){
 	return num1;
-	}else if(num2 > num1){
+	}else if(num1 < num2){
 	return num2;
 	}else{
 	printf("Error\n");
@@ -153,8 +174,10 @@ int encontrar_maximo(int num1, int num2){
 	}	 
 }
 
-double convertir_celsius_a_fahrenheit(double celsius){
+//retorna una multiplicacion simple
 
+double convertir_celsius_a_fahrenheit(double celsius){
+	
 	 return celsius * 1.8 + 32;
 
 }
@@ -166,31 +189,14 @@ void intercambiar_numeros() {
 
 double calcular_potencia(double base, int exponente){
 
-	return pow(base, exponente); // al usar la libreria de math hay que añadir un -lm al final de gcc.
+	//seria como decir: si el exponente es un 0 el resultado siempre sera un 1. Realmente no entiendo muy bien el codigo he tenido que buscarlo pero con pow seria muchisimo mas simple y te lo he dejado abajo.
 
-}
+	         if (exponente == 0) {
+	         return 1.0;
+	         }
+	       
+	         return base * calcular_potencia(base, exponente - 1);
+	                             }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	//return pow(base, exponente); // al usar la libreria de math hay que añadir un -lm al final de gcc.
 
