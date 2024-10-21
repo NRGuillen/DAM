c4401da45d210553dcacb6d62971ce4b7f43cdc1
NRGuillen <ruben.guillen.dosa@gmail.com>
2024-10-21 17:57:29 +0200

    Desde casa

--- Programación/EjerciciosDeClase/MurcielagoCadenaCaracteres.c
+++ Programación/EjerciciosDeClase/MurcielagoCadenaCaracteres.c
@@ -4,22 +4,37 @@
 int main(){
 
 	char a[10];
-	char b[10];
-	char c[10];
-	char d[10];
+	int b;
+	char c;
+	
 
 	printf("Introduzca una palabra: ");
 	scanf("%s", a);
 
 	printf("¿Que posicion desea modificar?");
-	scanf("%s", b);
+	scanf("%d", &b);
 
-	printf("¿Que caracte desea poner?");
-	scanf("%s", c);
+	printf("¿Que caracter desea poner?");
+	scanf(" %c", &c);
 
-	printf("La nueva palabra es: ");
-	scanf("%s", d);
+	a[b] = c;
+
+               	//la "a" seria nuestro indice de la palabra que introduzcamos en el char a. Por ejemplo
+	  	  //introduzco la palabra hola, "a" = hola.
+		  //la "b" seria la posicion de nuestro indice que queremos modificar. Por ejemplo 
+		  //introduzco el numero 2, se modificaria la letra "l".
+		  //'0' = h
+		  //'1' = o
+		  //'2' = l
+		  //'3' = a
+		  //'4' = \0
+		  //y la "c" es la que se encargar de reemplazar el nuevo caracter que hayamos introducidoi
+
+	printf("La nueva palabra es: %s\n", a);
 	
+	//en este printf se pone la "a" y no la "c" debido a que la variable "a" es la que contiene la palabra 
+	//completa y la "c" solo contiene el caracter que se va a cambiar, aparte de que un char a secas solo 
+	//puede printear una sola letra.
 	
 
 	return 0;
