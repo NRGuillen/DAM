50c9569cdc35b532a4a87234566b97fcdccfffb3
NRGuillen <ruben.guillen.dosa@gmail.com>
2024-10-22 20:00:13 +0200

    Ejercicio de animales terminado casa

--- Programación/EjerciciosDeClase/AnimalesCaracter.c
+++ Programación/EjerciciosDeClase/AnimalesCaracter.c
@@ -1,29 +1,31 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main(){
+int main() {
+
+	char opcion;
 	
-	char dibujo[5];
+	printf("¿Que quieres un gato o un perro? (g para gato, p para perro): ");
+	scanf(" %c", &opcion);
+
+	if(opcion == 'g'|| opcion == 'G' ){
+		   printf(" /\\_/\\\n");
+		   printf("( o.o )\n");
+		   printf(" > ^ <\n");
+	}
+
+	else if(opcion == 'p' || opcion == 'P'){
+		     printf(" / \\__\n");
+	             printf("(    @\\___\n");
+	             printf(" /         O\n");
+	             printf("/   (_____/\n");
+		     printf("/_____/   U\n");
+	}
+	else{
+		printf("No has introducido ni g ni p");
+	}
 	
-	printf("Introduce que dibujo quieres ver (perro o gato)");
-	scanf("%s", dibujo;
+	return 0;
 
-	if(dibujo if (dibujo[0] == 'g' && dibujo[1] == 'a' && dibujo[2] == 't' && dibujo[3] == 'o' && dibujo[4] == '\0') {
-        printf("  /^ ^\\\n");
-        printf(" / 0 0 \\\n");
-        printf(" V\\ Y /V\n");
-        printf("  / - \\\n");
-        printf(" /    |\n");
-        printf("V__) ||\n");
-    } else if (dibujo[0] == 'p' && dibujo[1] == 'e' && dibujo[2] == 'r' && dibujo[3] == 'r' && dibujo[4] == 'o' && dibujo[5] == '\0') {
-        printf("   __\n");
-        printf("o-''|\\_____/)\n");
-        printf(" \\_/|_)     )\n");
-        printf("    \\  __  /\n");
-        printf("    (_/ (_/\n");
-    } else {
-        printf("No has introducido ni gato ni perro\n");
-    }
+}
+	
 
-    return 0;
-    }
