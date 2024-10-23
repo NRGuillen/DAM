7d34bda776cbb33d1e4e50bf1fa5666d9a08c09e
Ruben Guillen <ruben.guillen.dosa@gmail.com>
2024-10-23 14:24:54 +0200

    Ejerecicios de clases

--- blucles/ejercicioclase3.c
+++ blucles/ejercicioclase3.c
@@ -3,13 +3,25 @@
 
 int main(){
 	
-	int numeros = 1;
+	int numeros = 0;
+ 	int suma = 0;// le da un valor por el que iniciar	
 
-	for(numeros = 1; numeros<=9; numeros = numeros += 1){
-		printf("%d+", numeros);
+	for(numeros = 0; numeros <=10; numeros = numeros +1 ){
+		printf("%d + %d = %d\n", suma, numeros, suma+numeros); //El printf se pone delante de la operacion para que lea primero la suma( seria el numero que da de resultado en la operacion anterior), luego lee el numero que sigue la cade de 1, 2, 3, 4, 5, 6, 7, 8, 9 y 10  y por ultimo da el resultado de la suma y el numero 
+		suma += numeros;	
 	}
-
-	printf("=%d.\n", numeros++);
-
+	printf("La suma total es:%d\n", suma);
+	
 	return 0;
 }
+
+// SOLUCION PROFESOR
+//#include <stdio.h>
+//int main() {
+//    	int suma = 0;
+//      for (int i = 1; i <= 10; i++) {
+//            suma += i;
+//       }
+//       printf("La suma es: %d\n", suma);
+//       return 0;
+//       }
