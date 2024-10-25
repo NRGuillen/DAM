4f362e367c9d826eeb8f0912d24da2323f5019d1
Ruben Guillen <ruben.guillen.dosa@gmail.com>
2024-10-25 12:59:27 +0200

    a

---
+++ while.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+	while(){
+
+		//codigo  a ejecutar
+
+		printf("Introduce una opcion: (4 para salir)\n");
+		scanf("%d");
+	}
+
+	return 0;
+}
