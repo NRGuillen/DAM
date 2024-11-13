99ef3ac6457b0fac1d2a186d20ed76920a4fb30d
Ruben Guillen <ruben.guillen.dosa@gmail.com>
2024-11-13 14:30:25 +0100

    asd

---
+++ Programación/punteros/EjerciciosClase/ejercicio4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+	void intercambio(int num1, int num2, int num3){
+
+
+		num1 = num3;
+		num2 = num1;
+		num1 = num2;
+	
+		printf("Numeros cambiados: %d y %d", num2, num1);
+	}
+	int main(){
+		int num1 = 10;
+		int num2= 5;
+		int num3;
+		intercambio(num1, num2, num3);  
+	
+	return 0;
+}
+
