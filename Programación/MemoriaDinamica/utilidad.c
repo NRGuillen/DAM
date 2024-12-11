e474426f56a74bf8cbf86b1343a7992fcf3967aa
Ruben Guillen <ruben.guillen.dosa@gmail.com>
2024-12-11 14:13:50 +0100

    Miercoles

--- Programación/MemoriaDinamica/utilidad.c
+++ Programación/MemoriaDinamica/utilidad.c
@@ -14,13 +14,42 @@ if (arr == NULL) {
 }
 
     for (int i = 0; i < n; i++) {
-        arr[i] = 2*i+1;
+        arr[i] = 2*i;
     }
-    
+   
 int * arr_ampliado = realloc(arr,100*sizeof(int));
 
 if (arr_ampliado == NULL) {
-        free(arr); 
+        free(arr); //Realloc no libera la memoria antigua.
+        
+        /*
+        Se pone dentro porque si realloc falla, el programa tiene que liberar 
+        la memoria reservada por lo tanto si hay un error antes de terminar el 
+        programa libero todo lo que haya.
+        */
+        
+        printf("Error: no se pudo asignar memoria.\n");
+        return EXIT_FAILURE;
+
+}
+
+arr = arr_ampliado; 
+
+for (int i = 0; i < n; ++i){
+    arr[i] = 2*i;
+    printf("arr[%d]: %d\n", i, arr_ampliado[i]);
+}
+
+int * arr_impar = realloc(arr,100*sizeof(int));
+
+if (arr_ampliado == NULL) {
+        free(arr); //Realloc no libera la memoria antigua.
+        
+        /*
+        Se pone dentro porque si realloc falla, el programa tiene que liberar 
+        la memoria reservada por lo tanto si hay un error antes de terminar el 
+        programa libero todo lo que haya.
+        */
         
         printf("Error: no se pudo asignar memoria.\n");
         return EXIT_FAILURE;
@@ -31,7 +60,7 @@ arr = arr_ampliado;
 
 for (int i = 0; i < n; ++i){
     arr[i] = 2*i+1;
-    printf("arr[%d]: %d\n", i, arr_ampliado[i]);
+    printf("arr[%d]: %d\n", i, arr_impar[i]);
 }
 
 
@@ -39,3 +68,4 @@ free(arr_ampliado);
 
 return 0;
 }
+
